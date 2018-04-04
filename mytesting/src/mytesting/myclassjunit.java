5d5fe394f285c9cae7f293ea40c27d2feb647f26
Alexandria <Alexandria@LAPTOP-2DP9U576.attlocal.net>
2018-04-04 01:52:31 -0500

    First commit
    Uploading all files

---
+++ mytesting/src/mytesting/myclassjunit.java
@@ -0,0 +1,14 @@
+package mytesting;
+
+
+public class myclassjunit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+		System.out.println(Math.pow(2, 8));
+	
+
+	}
+
+}
---
+++ mytesting/src/mytesting/myjunitclasstest.java
@@ -0,0 +1,14 @@
+package mytesting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class myjunitclasstest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
