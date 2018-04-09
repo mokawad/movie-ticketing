5d980261c3e4578e1e3a566b9c5497eed75ddb92
Alejandro Guzman Vega <aguzman-vega@csumb.edu>
2018-04-08 18:36:59 -0700

    Remove annotation that breaks the web page.

--- src/main/java/edu/csumb/xtreme/movieticketing/Application.java
+++ src/main/java/edu/csumb/xtreme/movieticketing/Application.java
@@ -3,7 +3,7 @@ package edu.csumb.xtreme.movieticketing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"edu.csumb.xtreme.movieticketing.dao"})
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
