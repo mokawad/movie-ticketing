b78d08757247dc8aaba36928a132bde090d70e86
mokawad <31768193+mokawad@users.noreply.github.com>
2018-04-14 21:51:35 -0500

    Updating Theater class

--- src/main/java/edu/csumb/xtreme/movieticketing/dao/TheaterDao.java
+++ src/main/java/edu/csumb/xtreme/movieticketing/dao/TheaterDao.java
@@ -6,5 +6,43 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TheaterDao extends JpaRepository<TheaterEntity, Integer> {
+public class TheaterList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TheaterList() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String movie_name=request.getParameter("movie");
+		movie_name=movie_name.substring(0,movie_name.length()-5);
+		List<String> list = new ArrayList<String>();
+        String json = null;
+		try {
+			list=AppUtils.listTheaters(movie_name);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		json = new Gson().toJson(list);
+        response.setContentType("application/json");
+        response.getWriter().write(json);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
 
 }
