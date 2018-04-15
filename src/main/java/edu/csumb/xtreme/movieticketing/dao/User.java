2b34d4ecc526ced13384c7f0fe1b1888d33a666a
mokawad <31768193+mokawad@users.noreply.github.com>
2018-04-14 21:57:23 -0500

    Creating User.java

---
+++ src/main/java/edu/csumb/xtreme/movieticketing/dao/User.java
@@ -0,0 +1,20 @@
+package dao;
+
+public class User {
+	private static String USER_NAME;
+	private static String PASSWORD;
+	public static String getUSER_NAME() {
+		return USER_NAME;
+	}
+	public static void setUSER_NAME(String uSER_NAME) {
+		USER_NAME = uSER_NAME;
+	}
+	public static String getPASSWORD() {
+		return PASSWORD;
+	}
+	public static void setPASSWORD(String pASSWORD) {
+		PASSWORD = pASSWORD;
+	}
+	
+
+}
