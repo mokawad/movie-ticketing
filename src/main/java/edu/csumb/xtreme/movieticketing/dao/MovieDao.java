36fc988e2154cdd54f4c600811bce99dec2bfe2e
mokawad <31768193+mokawad@users.noreply.github.com>
2018-04-14 21:55:02 -0500

    Updating Movie entity

--- src/main/java/edu/csumb/xtreme/movieticketing/dao/MovieDao.java
+++ src/main/java/edu/csumb/xtreme/movieticketing/dao/MovieDao.java
@@ -6,5 +6,60 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieDao extends JpaRepository<MovieEntity, Integer> {
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import dao.AppUtils;
+
+/**
+ * Servlet implementation class MovieList
+ */
+public class MovieList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MovieList() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<String> list = new ArrayList<String>();
+        String json = null;
+        
+        try {
+			list=AppUtils.listMovies();
+        } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+        json = new Gson().toJson(list);
+        response.setContentType("application/json");
+        response.getWriter().write(json);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
 
 }
